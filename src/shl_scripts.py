2fedc80eead79ab5ceb2161b9b7bede64f66c41e
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-10-21 15:08:02 +0200

    example script for scikit-learn

--- src/shl_scripts.py
+++ src/shl_scripts.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 from __future__ import division, print_function, absolute_import
+"""
+========================================================
+Learning filters from natural images using sparse coding
+========================================================
+
+* When imposing a code representing patches from natural images to be sparse,
+one observes the formation of filters ressembling the receptive field of simple
+cells in primates primary visual cortex.  This was first proposed in the
+framework of the SparseNet algorithm from Bruno Olshausen
+(http://redwood.berkeley.edu/bruno/sparsenet/).
+
+* This particular implementation has been published as Perrinet, Neural
+Computation (2010) (see http://invibe.net/LaurentPerrinet/Publications/Perrinet10shl )::
+
+   @article{Perrinet10shl,
+        Author = {Perrinet, Laurent U.},
+        Title = {Role of homeostasis in learning sparse representations},
+        Year = {2010}
+        Url = {http://invibe.net/LaurentPerrinet/Publications/Perrinet10shl},
+        Doi = {10.1162/neco.2010.05-08-795},
+        Journal = {Neural Computation},
+        Volume = {22},
+        Number = {7},
+        Keywords = {Neural population coding, Unsupervised learning, Statistics of natural images, Simple cell receptive fields, Sparse Hebbian Learning, Adaptive Matching Pursuit, Cooperative Homeostasis, Competition-Optimized Matching Pursuit},
+        Month = {July},
+        }
+
+
+"""
 
 import time
 import sys
@@ -175,3 +204,4 @@ if __name__ == '__main__':
     dico = shl.learn_dico()
     fig, ax = shl.show_dico(dico)
     plt.savefig('assc.png')
+    plt.show()
