4407f19fd261a8cd177808985b00be939230d524
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-03-17 15:46:18 +0100

    renaming to shl_encode

---
+++ shl_scripts/shl_encode.py
@@ -0,0 +1,162 @@
+import numpy as np
+
+
+def sparse_encode(X, dictionary, algorithm='mp', fit_tol=None,
+                          mod=None, l0_sparseness=10, verbose=0):
+    """Generic sparse coding
+
+    Each column of the result is the solution to a sparse coding problem.
+
+    Parameters
+    ----------
+    X : array of shape (n_samples, n_pixels)
+        Data matrix.
+
+    dictionary : array of shape (n_dictionary, n_pixels)
+        The dictionary matrix against which to solve the sparse coding of
+        the data. Some of the algorithms assume normalized rows.
+
+
+    algorithm : {'mp', 'lasso_lars', 'lasso_cd', 'lars', 'omp', 'threshold'}
+        mp :  Matching Pursuit
+        lars: uses the least angle regression method (linear_model.lars_path)
+        lasso_lars: uses Lars to compute the Lasso solution
+        lasso_cd: uses the coordinate descent method to compute the
+        Lasso solution (linear_model.Lasso). lasso_lars will be faster if
+        the estimated dictionary are sparse.
+        omp: uses orthogonal matching pursuit to estimate the sparse solution
+        threshold: squashes to zero all coefficients less than regularization
+        from the projection dictionary * data'
+
+    max_iter : int, 1000 by default
+        Maximum number of iterations to perform if `algorithm='lasso_cd'`.
+
+    verbose : int
+        Controls the verbosity; the higher, the more messages. Defaults to 0.
+
+    Returns
+    -------
+    code : array of shape (n_samples, n_dictionary)
+        The sparse codes
+
+    """
+    if X.ndim == 1:
+        X = X[:, np.newaxis]
+    #n_samples, n_pixels = X.shape
+
+    if algorithm == 'lasso_lars':
+        alpha = float(regularization) / n_pixels  # account for scaling
+
+        from sklearn.linear_model import LassoLars
+
+        # Not passing in verbose=max(0, verbose-1) because Lars.fit already
+        # corrects the verbosity level.
+        cov = np.dot(dictionary, X.T)
+        lasso_lars = LassoLars(alpha=fit_tol, fit_intercept=False,
+                               verbose=verbose, normalize=False,
+                               precompute=None, fit_path=False)
+        lasso_lars.fit(dictionary.T, X.T, Xy=cov)
+        sparse_code = lasso_lars.coef_.T
+
+    elif algorithm == 'lasso_cd':
+        alpha = float(regularization) / n_pixels  # account for scaling
+
+        # TODO: Make verbosity argument for Lasso?
+        # sklearn.linear_model.coordinate_descent.enet_path has a verbosity
+        # argument that we could pass in from Lasso.
+        from sklearn.linear_model import Lasso
+        clf = Lasso(alpha=fit_tol, fit_intercept=False, normalize=False,
+                    precompute=None, max_iter=max_iter, warm_start=True)
+
+        if init is not None:
+            clf.coef_ = init
+
+        clf.fit(dictionary.T, X.T, check_input=check_input)
+        sparse_code = clf.coef_.T
+
+    elif algorithm == 'lars':
+
+        # Not passing in verbose=max(0, verbose-1) because Lars.fit already
+        # corrects the verbosity level.
+        from sklearn.linear_model import Lars
+        cov = np.dot(dictionary, X.T)
+        lars = Lars(fit_intercept=False, verbose=verbose, normalize=False,
+                    precompute=None, n_nonzero_coefs=l0_sparseness,
+                    fit_path=False)
+        lars.fit(dictionary.T, X.T, Xy=cov)
+        sparse_code = lars.coef_.T
+
+    elif algorithm == 'threshold':
+        cov = np.dot(dictionary, X.T)
+        sparse_code = ((np.sign(cov) *
+                    np.maximum(np.abs(cov) - regularization, 0))).T
+
+    elif algorithm == 'omp':
+        # TODO: Should verbose argument be passed to this?
+        from sklearn.linear_model import orthogonal_mp_gram
+        from sklearn.utils.extmath import row_norms
+
+        cov = np.dot(dictionary, X.T)
+        gram = np.dot(dictionary, dictionary.T)
+        sparse_code = orthogonal_mp_gram(
+            Gram=gram, Xy=cov, n_nonzero_coefs=l0_sparseness,
+            tol=None, norms_squared=row_norms(X, squared=True),
+            copy_Xy=False).T
+
+    elif algorithm == 'mp':
+        sparse_code = mp(X, dictionary, l0_sparseness=l0_sparseness, fit_tol=fit_tol)
+    else:
+        raise ValueError('Sparse coding method must be "mp", "lasso_lars" '
+                         '"lasso_cd",  "lasso", "threshold" or "omp", got %s.'
+                         % algorithm)
+    return sparse_code
+
+def mp(X, dictionary, l0_sparseness=10, fit_tol=None, verbose=0):
+    """
+    Matching Pursuit
+    cf. https://en.wikipedia.org/wiki/Matching_pursuit
+
+    Parameters
+    ----------
+    X : array of shape (n_samples, n_pixels)
+        Data matrix.
+
+    dictionary : array of shape (n_dictionary, n_pixels)
+        The dictionary matrix against which to solve the sparse coding of
+        the data.
+
+    Returns
+    -------
+    sparse_code : array of shape (n_samples, n_dictionary)
+        The sparse code
+
+    """
+
+    # if mod is not None:
+    #     n_components, n_samples = mod.shape
+    #     z = np.empty(n_components)
+    # while True:
+    #     if mod is None:
+    #         lam = np.argmax(np.abs(alpha))
+    #     else:
+    #         for k in range(n_components):
+    #             z[k] = np.interp(-np.abs(alpha[k]), -mod[:, k], np.linspace(0, 1., n_samples, endpoint=True))
+    #         lam = np.argmax(z)
+    if X.ndim == 1:
+        X = X[:, np.newaxis]
+    n_samples, n_pixels = X.shape
+    n_dictionary, n_pixels = dictionary.shape
+    sparse_code = np.zeros((n_samples, n_dictionary))
+
+    corr = (X @ dictionary.T)
+    Xcorr = (dictionary @ dictionary.T)
+    # TODO: vectorize?
+    for i_sample in range(n_samples):
+        c = corr[i_sample, :].copy()
+        for i_l0 in range(int(l0_sparseness)):
+            ind  = np.argmax(np.abs(c))
+            c_ind = c[ind] / Xcorr[ind, ind]
+            sparse_code[i_sample, ind] += c_ind
+            c -= c_ind * Xcorr[ind, :]
+
+    return sparse_code
