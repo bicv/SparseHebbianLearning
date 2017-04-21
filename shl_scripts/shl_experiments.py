e9b9f41f51fdac62e326d14ef94860e0886e1eca
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-04-21 10:08:44 +0200

    headers

--- shl_scripts/shl_experiments.py
+++ shl_scripts/shl_experiments.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*
 from __future__ import division, print_function, absolute_import
 from shl_scripts.shl_tools import get_data
--- shl_scripts/shl_tools.py
+++ shl_scripts/shl_tools.py
@@ -1,4 +1,7 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+from __future__ import division, print_function, absolute_import
 from SLIP import Image
 from scipy.stats import kurtosis
 import sys
