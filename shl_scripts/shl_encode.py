89dca9ebbf13354356048653b09495894de1066f
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-12-01 13:21:30 +0100

    fixing the way I do the linear interpolation
    accounting for all special cases on the borders...

--- shl_scripts/shl_encode.py
+++ shl_scripts/shl_encode.py
@@ -160,11 +160,18 @@ def quantile(P_cum, p_c, stick, do_fast=True):
 
     """
     if do_fast:
-        #return P_cum.ravel()[(p_c*P_cum.shape[1] - (p_c==1)).astype(np.int) + stick]
-        indices = (p_c * P_cum.shape[1]).astype(np.int)
-        p = p_c * P_cum.shape[1] - indices
-        floor = P_cum.ravel()[indices - (p_c == 1) + stick]
-        ceil = P_cum.ravel()[indices + 1 - 2 * (p_c == 1) + stick]
+        try:
+            indices = (p_c * shl.nb_quant).astype(np.int)  # (floor) index of each p_c in the respective line of P_cum
+            p = p_c * shl.nb_quant - indices  # ratio between floor and ceil
+            floor = P_cum.ravel()[
+                indices - (p_c == 1) + stick]  # floor, accounting for extremes, and moved on the raveled P_cum matrix
+            ceil = P_cum.ravel()[indices + 1 - (p_c == 0) - (p_c == 1) -
+                                (indices >= P_cum.shape[1] - 1) + stick]  # ceiling,  accounting for both extremes, and moved similarly
+        except IndexError as e:
+            print (e)
+            print(P_cum.shape, np.prod(P_cum.shape), p_c, floor, p,
+                  indices - (p_c == 1) + stick,
+                  indices + 1 - (p_c == 0) - (p_c == 1) - (indices >= stick + P_cum.shape[1] - 1) + stick)
         return (1 - p) * floor + p * ceil
     else:
         code_bins = np.linspace(0., 1., P_cum.shape[1], endpoint=True)
