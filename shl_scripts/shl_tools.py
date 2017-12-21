96354ee7d824069f1ebcfead1f702454ec9ae61d
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-12-21 11:35:36 +0100

    a different normalization
    the "using tools" notebook was showing that the previous version generated binary images

--- shl_scripts/shl_tools.py
+++ shl_scripts/shl_tools.py
@@ -58,14 +58,14 @@ def get_data(height=256, width=256, n_image=200, patch_size=(12,12),
             # whitening
             image, filename_, croparea_ = slip.patch(name_database, filename=filename, croparea=croparea, center=False)
             image = slip.whitening(image)
+
             # Extract all reference patches and ravel them
             data_ = slip.extract_patches_2d(image, patch_size, N_patches=int(max_patches))
+            data_ -= np.mean(data_)
+            if patch_norm:
+                data_ /= np.std(data_)
 
             data_ = data_.reshape(data_.shape[0], -1)
-            data_ -= np.mean(data_, axis=0)
-            if patch_norm:
-                data_ /= np.std(data_, axis=0)
-
             if do_mask:
                 data_ = data_ * mask[np.newaxis, :]
 
