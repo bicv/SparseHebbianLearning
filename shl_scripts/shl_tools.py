a4e088781030a6107839f608d8f3fcfce38b36ed
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-03-17 15:41:58 +0100

    print stats function

--- shl_scripts/shl_tools.py
+++ shl_scripts/shl_tools.py
@@ -184,48 +184,42 @@ def get_logL(sparse_code):
 
 def print_stats(data, dictionary, sparse_code, max_patches=10):
     import matplotlib.pyplot as plt
-
     print(42*'🐒')
-    patches = sparse_code @ dictionary
-    error = data - patches
 
     print('number of codes, size of codewords = ', sparse_code.shape)
     print('average of codewords = ', sparse_code.mean())
-    print('l0-sparseness of codewords = ', (sparse_code>0).mean())#, ' ~= l0/M =', shl.l0_sparseness/shl.n_dictionary)
-    print('average energy of codewords = ', sparse_code.std(axis=0).mean())
     print('average std of codewords = ', sparse_code.std())
+    print('l0-sparseness of codewords = ', (sparse_code>0).mean(), ' ~= l0/M =', shl.l0_sparseness/shl.n_dictionary)
     print('std of the average of individual patches = ', sparse_code.mean(axis=0).std())
 
-    print('number of codes, size of reconstructed images = ', patches.shape)
 
     plt.matshow(sparse_code[:N_show, :])
     plt.show()
     fig, axs = show_data(data[:max_patches, :])
     plt.show()
+
+    patches = sparse_code @ dictionary
+    error = data - patches
+
+    print('number of codes, size of reconstructed images = ', patches.shape)
+
     fig, axs = show_data(patches[:max_patches, :])
     plt.show()
-
-    fig, axs = show_data(error[:max_patches, :], cmax=np.max(np.abs(data[:max_patches, :])))
+    fig, axs = show_data(error[:max_patches, :], cmax=np.max(np.abs(patches[:max_patches, :])))
     plt.show()
     print('average of data patches = ', data.mean(), '+/-', data.mean(axis=1).std())
     print('average of residual patches = ', error.mean(), '+/-', error.mean(axis=1).std())
     SD = np.sqrt(np.mean(data**2, axis=1))
-    #SD = np.linalg.norm(data[indx, :])/record_num_batches
 
     print('median energy of data = ', np.median(SD))
     print('average energy of data = ', SD.mean(), '+/-', SD.std())
-    #print('total energy of data = ', np.sqrt(np.sum(data**2)))
-    #print('total deviation of data = ', np.sum(np.abs(data)))
     print('average error = ', error.mean(), '+/-', error.std())
+
     SE = np.sqrt(np.mean(error**2, axis=1))
-    #SE = np.linalg.norm(error)/record_num_batches
 
     print('average energy of residual = ', SE.mean(), '+/-', SE.std())
     print('median energy of residual = ', np.median(SE))
-    #print('total energy of residual = ', np.sqrt(np.sum(error**2)))
-    #print('total deviation of residual = ', np.sum(np.abs(error)))
     print('average gain of coding = ', (SD/SE).mean(), '+/-', (SD/SE).std())
-
 
     return SD, SE
 
